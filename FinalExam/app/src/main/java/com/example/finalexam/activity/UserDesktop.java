48a7a5f0a44ad074c4502ab9acaa445e7f340a3e
PPPoria <1316543327@qq.com>
2024-08-11 16:43:34 +0800

    完善了个人页的布局

--- FinalExam/app/src/main/java/com/example/finalexam/activity/UserDesktop.java
+++ FinalExam/app/src/main/java/com/example/finalexam/activity/UserDesktop.java
@@ -3,7 +3,7 @@ package com.example.finalexam.activity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
 import android.widget.FrameLayout;
 
 import androidx.activity.EdgeToEdge;
@@ -21,12 +21,9 @@ import com.example.finalexam.fragment.ProjectFragment;
 import com.example.finalexam.overrideview.AddButton;
 
 public class UserDesktop extends BaseActivity {
-    private static final String TAG = "UserDesktop";
 
-    private FragmentManager fragmentManager;
-    private FrameLayout container;
-    private ProjectFragment projectFragment;
-    private PersonFragment personFragment;
+    private FrameLayout projectContainer;
+    private FrameLayout personContainer;
     private int pagePosition = 0;
 
     private AddButton addButton;
@@ -52,48 +49,42 @@ public class UserDesktop extends BaseActivity {
 
     @SuppressLint("CommitTransaction")
     private void initPage() {
-        projectFragment = new ProjectFragment();
-        personFragment = new PersonFragment();
+        ProjectFragment projectFragment = new ProjectFragment();
+        PersonFragment personFragment = new PersonFragment();
 
-        fragmentManager = getSupportFragmentManager();
+        FragmentManager fragmentManager = getSupportFragmentManager();
 
         FragmentTransaction transaction = fragmentManager.beginTransaction();
-        transaction.add(R.id.user_pagerContainer, projectFragment);
+        transaction.add(R.id.user_project_container, projectFragment);
+        transaction.add(R.id.user_person_container, personFragment);
         transaction.commit();
     }
 
     private void initListener() {
-        addButton.setOnClickListener(v->{
-            startActivity(new Intent(this, AddProjectActivity.class));
-        });
-        projectPageButton.setOnClickListener(v->{
-            changePageTo(0);
-        });
-        personPageButton.setOnClickListener(v->{
-            changePageTo(1);
-        });
+        addButton.setOnClickListener(v -> startActivity(new Intent(this, AddProjectActivity.class)));
+        projectPageButton.setOnClickListener(v -> changePageTo(0));
+        personPageButton.setOnClickListener(v -> changePageTo(1));
     }
 
     private void initView() {
-        container = findViewById(R.id.user_pagerContainer);
+        projectContainer = findViewById(R.id.user_project_container);
+        personContainer = findViewById(R.id.user_person_container);
         addButton = findViewById(R.id.add_button);
         projectPageButton = findViewById(R.id.user_project_page_button);
         personPageButton = findViewById(R.id.user_person_page_button);
     }
 
-    private void changePageTo(int pagePosition){
+    private void changePageTo(int pagePosition) {
         //防止浪费算力或丢失已经绘制的画面
         if (this.pagePosition == pagePosition) return;
         else this.pagePosition = pagePosition;
 
-        FragmentTransaction transaction = fragmentManager.beginTransaction();
-        if (pagePosition == 0){
-            transaction.replace(R.id.user_pagerContainer, projectFragment);
-            Log.d(TAG, "UserDesktop change page to ProjectFragment");
-        }else{
-            transaction.replace(R.id.user_pagerContainer, personFragment);
-            Log.d(TAG, "UserDesktop change page to PersonFragment");
+        if (pagePosition == 0) {
+            projectContainer.setVisibility(View.VISIBLE);
+            personContainer.setVisibility(View.INVISIBLE);
+        } else {
+            projectContainer.setVisibility(View.INVISIBLE);
+            personContainer.setVisibility(View.VISIBLE);
         }
-        transaction.commit();//
     }
 }
--- FinalExam/app/src/main/java/com/example/finalexam/fragment/PersonFragment.java
+++ FinalExam/app/src/main/java/com/example/finalexam/fragment/PersonFragment.java
@@ -1,21 +1,271 @@
 package com.example.finalexam.fragment;
 
+import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.finalexam.R;
-public class PersonFragment extends Fragment {
+import com.example.finalexam.activity.LogActivity;
+import com.example.finalexam.activity.MyApplyActivity;
+import com.example.finalexam.activity.OtherApplyActivity;
+import com.example.finalexam.helper.ColorHelper;
+import com.example.finalexam.helper.UserDataShowInterface;
+import com.example.finalexam.model.ProjectData;
+import com.example.finalexam.presenter.SPPresenter;
+import com.example.finalexam.presenter.UserPresenter;
+import com.example.finalexam.presenter.WebSocketPresenter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class PersonFragment extends Fragment implements UserDataShowInterface {
     private static final String TAG = "PersonFragment";
     private View view;
+
+    private TextView greetView;
+    private TextView colorView;
+    private TextView nameView;
+    private TextView logoutButton;
+    private ConstraintLayout toMineButton;
+    private TextView mineNumView;
+    private ConstraintLayout toOtherButton;
+    private TextView otherNumView;
+
+    private int requestNum = 0;
+    public static final List<ProjectData> myProjects = new ArrayList<>();
+    public static final List<ProjectData> myApplications = new ArrayList<>();
+    public static final List<ProjectData> otherApplications = new ArrayList<>();
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_person, container, false);
+
+        initView();
+        initListener();
+        requestData();
+
         return view;
+    }
+
+    private void showData() {
+        int hour = LocalDateTime.now().getHour();
+        String greet = "您好";
+        if (hour < 5) greet = "凌晨了";
+        else if (hour < 11) greet = "早上好";
+        else if (hour < 13) greet = "中午好";
+        else if (hour < 18) greet = "下午好";
+        else if (hour < 24) greet = "晚上好";
+        greetView.setText(greet);
+
+        String name = UserPresenter.getInstance(this).getUserName(UserPresenter.getContext());
+        name = name == null ? "null" : name;
+        nameView.setText(name);
+
+        int color = Color.parseColor(ColorHelper.createColorHex(name));
+        colorView.setText(name.substring(0, 1));
+        colorView.setBackgroundTintList(ColorStateList.valueOf(color));
+        if (!ColorHelper.isBrightColor(color))
+            colorView.setTextColor(Color.WHITE);
+    }
+
+    private void requestData() {
+        UserPresenter.getInstance(this).fetchMyApplyingProject();//我发布或更改的记录
+        UserPresenter.getInstance(this).fetchApplyingMonitorProject();//我申请监视他人项目的记录
+        UserPresenter.getInstance(this).fetchApplication();//他人申请监视我的项目的记录
+    }
+
+    private void initListener() {
+        logoutButton.setOnClickListener(v -> {
+            SPPresenter.accordLoggedStatus(UserPresenter.getContext(), false);
+            startActivity(new Intent(UserPresenter.getContext(), LogActivity.class));
+            int userId = UserPresenter.getInstance(this).getUserId();//关闭WebSocket
+            WebSocketPresenter.getInstance(UserPresenter.getContext()).getWebSocketClient(userId).close();
+            requireActivity().finish();
+        });
+        toMineButton.setOnClickListener(v -> {
+            startActivity(new Intent(UserPresenter.getContext(), MyApplyActivity.class));
+        });
+        toOtherButton.setOnClickListener(v -> {
+            startActivity(new Intent(UserPresenter.getContext(), OtherApplyActivity.class));
+        });
+    }
+
+    private void initView() {
+        greetView = view.findViewById(R.id.greet);
+        colorView = view.findViewById(R.id.person_color);
+        nameView = view.findViewById(R.id.person_name);
+        logoutButton = view.findViewById(R.id.person_logout_button);
+        toMineButton = view.findViewById(R.id.person_my_apply_layout);
+        mineNumView = view.findViewById(R.id.person_my_apply_num);
+        toOtherButton = view.findViewById(R.id.person_other_apply_layout);
+        otherNumView = view.findViewById(R.id.person_other_apply_num);
+    }
+
+    @Override
+    public void applyMonitorPermission(int STATUS) {
+
+    }
+
+    @Override
+    public void checkMonitorResult(int STATUS) {
+
+    }
+
+    @Override
+    public void freeze(int STATUS) {
+
+    }
+
+    @Override
+    public void projectPublishResult(int STATUS) {
+
+    }
+
+    @Override
+    public void briefProjectList(int STATUS) {
+
+    }
+
+    @Override
+    public void selfProjectList(int STATUS) {
+
+    }
+
+    @Override
+    public void monitorProjectList(int STATUS) {
+
+    }
+
+    @Override
+    public void applyingMonitorProjectList(int STATUS) {
+        if (STATUS == UserPresenter.STATUS_SUCCESS) {
+            myApplications.addAll(UserPresenter.getInstance(this).getProjectList());
+        }
+
+        if (++requestNum == 3) {
+            requestNum = 0;
+            showData();
+        }
+    }
+
+    @Override
+    public void applyingProjectList(int STATUS) {
+        if (STATUS == UserPresenter.STATUS_SUCCESS) {
+            myProjects.addAll(UserPresenter.getInstance(this).getProjectList());
+        }
+
+        if (++requestNum == 3) {
+            requestNum = 0;
+            showData();
+        }
+    }
+
+    @Override
+    public void projectDetail(int STATUS) {
+
+    }
+
+    @Override
+    public void updateProject(int STATUS) {
+
+    }
+
+    @Override
+    public void cancelMonitor(int STATUS) {
+
+    }
+
+    @Override
+    public void deleteProject(int STATUS) {
+
+    }
+
+    @Override
+    public void freezeOrNotProjectList(int STATUS) {
+
+    }
+
+    @Override
+    public void applyOrNotProjectList(int STATUS) {
+
+    }
+
+    @Override
+    public void userLog(int STATUS) {
+
+    }
+
+    @Override
+    public void userRegister(int STATUS) {
+
+    }
+
+    @Override
+    public void monitorUserListResult(int STATUS) {
+
+    }
+
+    @Override
+    public void userDetail(int STATUS) {
+
+    }
+
+    @Override
+    public void verify(int STATUS) {
+
+    }
+
+    @Override
+    public void application(int STATUS) {
+        if (STATUS == UserPresenter.STATUS_SUCCESS) {
+            otherApplications.addAll(UserPresenter.getInstance(this).getProjectList());
+        }
+
+        if (++requestNum == 3) {
+            requestNum = 0;
+            showData();
+        }
+    }
+
+    @Override
+    public void attackServerLogList(int STATUS) {
+
+    }
+
+    @Override
+    public void allUserOperationLogList(int STATUS) {
+
+    }
+
+    @Override
+    public void logDataListByGroup(int STATUS) {
+
+    }
+
+    @Override
+    public void projectPresentationDateOneWeek(int STATUS) {
+
+    }
+
+    @Override
+    public void ViewProjectOperateLog(int STATUS) {
+
+    }
+
+    @Override
+    public void increaseView(int STATUS) {
+
     }
 }
