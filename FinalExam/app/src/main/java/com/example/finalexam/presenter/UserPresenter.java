10bf97f76970ccfcdbd15630485975cf9535b0c0
NOIDEA8 <3086495963@qq.com>
2024-08-12 14:50:53 +0800

    改baseurl

--- FinalExam/app/src/main/java/com/example/finalexam/presenter/UserPresenter.java
+++ FinalExam/app/src/main/java/com/example/finalexam/presenter/UserPresenter.java
@@ -48,7 +48,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class UserPresenter {
     private static final String TAG = "UserPresenter";
-    private static final String baseUrl="http://47.113.224.195:31111/";
+    private static final String baseUrl="http://47.113.224.195:31101/";
     public UserDataShowInterface activity;
     private String token;
     private static UserPresenter presenter=new UserPresenter();
