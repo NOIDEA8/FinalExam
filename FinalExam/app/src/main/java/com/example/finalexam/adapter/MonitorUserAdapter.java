febebed006ad2ad6eeff545e42d8cf1846efb0a0
PPPoria <1316543327@qq.com>
2024-08-16 23:48:00 +0800

    实现了监视者查看

--- FinalExam/app/src/main/java/com/example/finalexam/adapter/MonitorUserAdapter.java
+++ FinalExam/app/src/main/java/com/example/finalexam/adapter/MonitorUserAdapter.java
@@ -1,72 +1,94 @@
 package com.example.finalexam.adapter;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
-import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.finalexam.R;
-import com.example.finalexam.activity.UserDetailActivity;
-import com.example.finalexam.activity.VerifyActivity;
 import com.example.finalexam.helper.ColorHelper;
-import com.example.finalexam.model.ProjectData;
 import com.example.finalexam.model.UserData;
 
 import java.util.List;
 
 public class MonitorUserAdapter extends RecyclerView.Adapter<MonitorUserAdapter.ItemHolder> {
-
-private static final String TAG = "MonitorUserAdapter";
-private List<UserData> userList;
-public static int clickId;
-private Context context;
+    private final List<UserData> list;
+    private final Context context;
+    private int num;
 
 
-public MonitorUserAdapter(Context context, List<UserData> userList) {
-    this.context = context;
-    this.userList=userList;
-}
-
-@NonNull
-@Override
-public MonitorUserAdapter.ItemHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-    View itemView = LayoutInflater.from(context).inflate(R.layout.project_rv_item, parent, false);
-    return new MonitorUserAdapter.ItemHolder(itemView);
-}
-
-@Override
-public void onBindViewHolder(@NonNull MonitorUserAdapter.ItemHolder holder, int position) {
-
-}
-
-@Override
-public int getItemCount() {
-    if (userList == null) return 0;
-    return userList.size();
-}
-
-public static class ItemHolder extends RecyclerView.ViewHolder {
-    public ConstraintLayout userRVItem;
-    public TextView userName;
-    public TextView online;
-    public TextView userColor;
-    public View offLine;
-    public ItemHolder(@NonNull View itemView) {
-        super(itemView);
-        userRVItem = itemView.findViewById(R.id.user_rv_item);
-        userName = itemView.findViewById(R.id.user_name_overview);
-        online = itemView.findViewById(R.id.online_overview);
-        userColor = itemView.findViewById(R.id.user_color);
-        offLine =itemView.findViewById(R.id.user_item_no_online);
+    public MonitorUserAdapter(Context context, List<UserData> list) {
+        this.context = context;
+        this.list = list;
     }
-}
+
+    @SuppressLint("NotifyDataSetChanged")
+    public void resetList() {
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public ItemHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(context).inflate(R.layout.monitor_user_item, parent, false);
+        return new ItemHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemHolder holder, int position) {
+        String name = list.get(position * 2).getUsername();
+        Log.d("Test", name);
+        String colorHex = ColorHelper.createColorHex(name);
+        int color = Color.parseColor(colorHex);
+        holder.userName1.setText(name);
+        holder.userColor1.setText(name.substring(0, 1));
+        holder.userColor1.setBackgroundTintList(ColorStateList.valueOf(color));
+        if (ColorHelper.isBrightColor(color))
+            holder.userColor1.setTextColor(Color.BLACK);
+        else
+            holder.userColor1.setTextColor(Color.WHITE);
+
+        if (position * 2 + 1 == num) return;
+
+        name = list.get(position * 2 + 1).getUsername();
+        colorHex = ColorHelper.createColorHex(name);
+        color = Color.parseColor(colorHex);
+        holder.userName2.setText(name);
+        holder.userColor2.setText(name.substring(0, 1));
+        holder.userColor2.setBackgroundTintList(ColorStateList.valueOf(color));
+        if (ColorHelper.isBrightColor(color))
+            holder.userColor2.setTextColor(Color.BLACK);
+        else
+            holder.userColor2.setTextColor(Color.WHITE);
+    }
+
+    @Override
+    public int getItemCount() {
+        num = list.size();
+        return num % 2 == 0 ? num / 2 : num / 2 + 1;
+    }
+
+    public static class ItemHolder extends RecyclerView.ViewHolder {
+        public TextView userColor1;
+        public TextView userName1;
+        public TextView userColor2;
+        public TextView userName2;
+
+        public ItemHolder(@NonNull View view) {
+            super(view);
+            userColor1 = view.findViewById(R.id.monitor_user1);
+            userName1 = view.findViewById(R.id.monitor_user1_name);
+            userColor2 = view.findViewById(R.id.monitor_user2);
+            userName2 = view.findViewById(R.id.monitor_user2_name);
+        }
+    }
 
 }
--- FinalExam/app/src/main/java/com/example/finalexam/fragment/MonitorUserFragment.java
+++ FinalExam/app/src/main/java/com/example/finalexam/fragment/MonitorUserFragment.java
@@ -6,19 +6,24 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.finalexam.R;
+import com.example.finalexam.activity.ProjectDetailActivity;
 import com.example.finalexam.adapter.MonitorUserAdapter;
 import com.example.finalexam.adapter.ProjectAdapter;
 import com.example.finalexam.helper.UserDataShowInterface;
+import com.example.finalexam.model.ProjectData;
 import com.example.finalexam.model.UserData;
 import com.example.finalexam.presenter.UserPresenter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class MonitorUserFragment extends Fragment implements UserDataShowInterface {
@@ -40,37 +45,33 @@ public class MonitorUserFragment extends Fragment implements UserDataShowInterfa
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         view=inflater.inflate(R.layout.fragment_monitor_user, container, false);
+        initView();
+        initRV();
+        requestData();
         return view;
     }
 
     private void requestData() {
-        UserPresenter.getInstance(this).fetchAllBriefProject();
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                UserPresenter.getInstance(MonitorUserFragment.this).fetchMonitorUser(ProjectAdapter.clickId);
+                Log.d("Test", "requestData: click id = " + ProjectAdapter.clickId);
+            }
+        };
+        timer.schedule(task,200);
     }
 
-
     private void initRV() {
-        MonitorUserRV.setLayoutManager(new LinearLayoutManager(requireContext()));
+        MonitorUserRV.setLayoutManager(new LinearLayoutManager(UserPresenter.getContext()));
         adapter = new MonitorUserAdapter(requireContext(),list);
         MonitorUserRV.setAdapter(adapter);
     }
 
     private void initView() {
-        MonitorUserRV = view.findViewById(R.id.project_list);
+        MonitorUserRV = view.findViewById(R.id.monitor_rv);
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     @Override
     public void applyMonitorPermission(int STATUS) {
@@ -159,7 +160,11 @@ public class MonitorUserFragment extends Fragment implements UserDataShowInterfa
 
     @Override
     public void monitorUserListResult(int STATUS) {
-
+        if (STATUS == UserPresenter.STATUS_SUCCESS){
+            list.clear();
+            list.addAll(UserPresenter.getInstance(this).getUserList());
+            adapter.resetList();
+        }
     }
 
     @Override
